f94f25b417d6400b13bb54f3b608e46af7a2b19d
Sean Sundberg <seansund@us.ibm.com>
2023-08-28 12:00:24 -0500

    Update UI titles and clean up menu/router code
    Signed-off-by: Sean Sundberg <seansund@us.ibm.com>

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import {Route, Routes} from "react-router-dom";
-import {Notification, Search, Switcher} from "@carbon/icons-react";
+import {Notification, Search, User} from "@carbon/icons-react";
 
 import './App.css'
 import {Dummy, NotFound, UIShell} from "./components";
@@ -9,36 +9,33 @@ import {NavigationModel} from "./models";
 
 function App() {
 
+    const menuLinks = [
+        {title: 'Dashboard', href: '/', element: <Dashboard />},
+        {title: 'KYC', href: '/kyc', element: <Dummy content="KYC" />},
+        {title: 'TM Alerts', href: '/tm-alerts', element: <Dummy content="TM Alerts" />},
+        {title: 'Customer Risk', href: '/customer-risk', element: <Dummy content="Customer Risk" />},
+        {title: 'RTC - QC', href: '/rtc-qc', element: <Dummy content="RTC - QC" />},
+        {title: 'Tax - CLM', href: '/tax-clm', element: <Dummy content="Tax - CLM" />},
+        {title: 'Tax - GTM', href: '/tax-gtm', element: <Dummy content="Tax - GTM" />},
+        {title: 'Utilities', href: '/utilities', element: <Dummy content="Utilities" />},
+    ]
+
     const navigation: NavigationModel = {
         globalBar: [
             {title: 'Search', action: (<Search size={20} />)},
             {title: 'Notifications', action: (<Notification size={20} />)},
-            {title: 'App Switcher', action: (<Switcher size={20} />)}
+            {title: 'User Profile', action: (<User size={20} />)}
         ],
-        sideNav: [
-            {title: 'Dashboard', href: '/'},
-            {title: 'KYC', href: '/kyc'},
-            {title: 'TM Alerts', href: '/tm-alerts'},
-            {title: 'Customer Risk', href: '/customer-risk'},
-            {title: 'RTC - QC', href: '/rtc-qc'},
-            {title: 'Tax - CLM', href: '/tax-clm'},
-            {title: 'Tax - GTM', href: '/tax-gtm'},
-            {title: 'Utilities', href: '/utilities'},
-        ]
+        sideNav: menuLinks.map(link => ({title: link.title, href: link.href}))
     }
 
   return (
     <div>
         <UIShell prefix="watsonx" navigation={navigation}>
             <Routes>
-                <Route path="/" element={<Dashboard />} />
-                <Route path="/kyc" element={<Dummy content="KYC" />} />
-                <Route path="/tm-alerts" element={<Dummy content="TM Alerts" />} />
-                <Route path="/customer-risk" element={<Dummy content="Customer Risk" />} />
-                <Route path="/rtc-qc" element={<Dummy content="RTC-QC" />} />
-                <Route path="/tax-clm" element={<Dummy content="Tax-CLM" />} />
-                <Route path="/tax-gtm" element={<Dummy content="Tax-GTM" />} />
-                <Route path="/utilities" element={<Dummy content="Utilities" />} />
+                {menuLinks.map(link => (
+                    <Route path={link.href} element={link.element} />
+                ))}
                 <Route path="*" element={<NotFound />} />
             </Routes>
         </UIShell>
