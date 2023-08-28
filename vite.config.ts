026cbe490b1b7772bd65a0ed6d4f65f83ce498ab
Sean Sundberg <seansund@us.ibm.com>
2023-08-28 13:16:57 -0500

    Add proxy config to vite.config.ts
    Signed-off-by: Sean Sundberg <seansund@us.ibm.com>

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,25 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
+
+const apiTarget: string = '' || 'http://localhost:3000';
+const socketTarget: string = '' || 'ws://localhost:3000';
+
 export default defineConfig({
   plugins: [react()],
+  server: {
+    proxy: {
+      // with options: http://localhost:5173/api/bar-> {apiTarget}/bar
+      '/api': {
+        target: apiTarget,
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, ''),
+      },
+      '/socket': {
+        target: socketTarget,
+        ws: true,
+        rewrite: (path) => path.replace(/^\/socket/, '')
+      },
+    },
+  },
 })
