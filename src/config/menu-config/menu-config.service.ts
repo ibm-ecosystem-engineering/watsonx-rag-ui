481550a144d8888531950f31b304ceabd9a17796
Sean Sundberg <seansund@us.ibm.com>
2023-09-13 16:33:00 -0400

    Add negative news screening url (#25)
    Signed-off-by: Sean Sundberg <seansund@us.ibm.com>

--- src/config/menu-config/menu-config.service.ts
+++ src/config/menu-config/menu-config.service.ts
@@ -6,7 +6,7 @@ export class MenuConfigService implements MenuConfigApi {
     async loadConfig(): Promise<MenuConfigModel> {
         return {
             dataExtractionUrl: '',
-            negativeNewsScreeningUrl: '',
+            negativeNewsScreeningUrl: 'https://negnews.161ia4n12g8f.us-south.codeengine.appdomain.cloud/',
             contractComplianceCheckUrl: '',
             qualitySamplingUrl: '',
             riskAssessmentEngineUrl: 'https://cpd-cp4ba.cp4ba-cra-c6c44da74def18a795b07cc32856e138-0000.us-south.containers.appdomain.cloud/ae-pbk/Risk%20Assessment%20Business%20Application(EKYCBA)',
