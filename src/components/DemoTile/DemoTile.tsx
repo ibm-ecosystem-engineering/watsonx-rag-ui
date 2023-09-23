d1a9c96cb6a197f737c6db77e2ee3a6e407d6fd3
Sean Sundberg <seansund@us.ibm.com>
2023-09-23 06:24:45 -0500

    Use Link for internal links instead of anchor (#80)
    closes #79
    
    Signed-off-by: Sean Sundberg <seansund@us.ibm.com>

--- src/components/DemoTile/DemoTile.tsx
+++ src/components/DemoTile/DemoTile.tsx
@@ -3,6 +3,7 @@
 // @ts-ignore
 import React, { Component } from 'react';
 import './DemoTile.scss';
+import {Link} from "react-router-dom";
 
 export interface DemoTileProps {
     title: string
@@ -13,6 +14,7 @@ export interface DemoTileState {
 }
 
 export class DemoTile extends Component<DemoTileProps, DemoTileState> {
+
 
   render() {
       if (this.props.href.startsWith('http')) {
@@ -28,13 +30,13 @@ export class DemoTile extends Component<DemoTileProps, DemoTileState> {
       }
 
       return (
-          <a className="demoTileLink" href={this.props.href}>
+          <Link className="demoTileLink" to={this.props.href}>
               <div className="demoTile">
                   <div className="demoTileContent">
                   {this.props.title}
                   </div>
               </div>
-          </a>
+          </Link>
       )
   }
 }
